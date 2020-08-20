5dbe18f06bfaa5849e4002429217b1c447324b93
Abdul Halim <a.halimics@gmail.com>
2020-08-20 14:21:29 +0600

    last update

--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Student;
-class Member extends Model
+use App\Models\Book;
+
+class Borrow extends Model
 {
-    protected $table = 'library_members';
+    protected $table = 'borrows';
 
     public function student(){
         return $this->belongsTo(Student::class);
+    }
+
+    public function book(){
+        return $this->belongsTo(Book::class);
     }
 }
